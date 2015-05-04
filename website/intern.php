04118544f68dd3a76c888a13a9e5092a62e49166
StormDoom <josefkruse@googlemail.com>
2015-05-04 20:07:50 +0200

    Update intern.php

--- website/intern.php
+++ website/intern.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION["username"]))
 { 
 echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
 exit; 
-}
+//}
  //  else
 //   {
  //     if($row->usergroup != "secure")
@@ -24,6 +24,6 @@ exit;
          {
          echo "Zum FTP geht es <a href=\"http://stormdoom.noip.me/ftp\">hier </a> lang.";
          }
-   }
+//   }
    exit; 
 ?> 
