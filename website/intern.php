e35646f670e83d3995e803ea1fbe772147f5308a
StormDoom <josefkruse@googlemail.com>
2015-05-04 20:21:04 +0200

    Update intern.php

--- website/intern.php
+++ website/intern.php
@@ -5,29 +5,29 @@ session_start();
 //$verbindung = mysql_connect("localhost", "homepage" , "homepage") 
 //or die("Verbindung zur Datenbank konnte nicht hergestellt werden"); 
 //mysql_select_db("homepage") or die ("Datenbank konnte nicht ausgewählt werden"); 
-echo $_SESSION['username']
+echo $_SESSION['username'];
 //$abfrage = "SELECT usergroup FROM login WHERE username LIKE $_SESSION["username"] LIMIT 1";
 //$ergebnis = mysql_query($abfrage);
 //$row = mysql_fetch_object($ergebnis);
-if(!isset($_SESSION["username"])) 
-{ 
-echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
-exit; 
-}
+//if(!isset($_SESSION["username"])) 
+//{ 
+//echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
+//exit; 
+//}
  //  else
 //   {
  //     if($row->usergroup != "secure")
    //   {
      // echo "Du bist noch nicht freigeschaltet. Bitte gedulde dich etwas."
    //   }
-        else
-         {
-          echo $abfrage;
-          echo $ergebnis
-          echo $row
-          echo $_SESSION["username"]
+  //      else
+    //     {
+      //    echo $abfrage;
+        //  echo $ergebnis
+         // echo $row
+          //echo $_SESSION["username"]
          //echo "Zum FTP geht es <a href=\"http://stormdoom.noip.me/ftp\">hier </a> lang.";
-         }
+         //}
 //   }
-   exit; 
+  // exit; 
 ?> 
