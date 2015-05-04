28273bf9ff288c86d3cad22d6b7e760cfcd93f16
StormDoom <josefkruse@googlemail.com>
2015-05-04 18:36:30 +0200

    Update intern.php

--- website/intern.php
+++ website/intern.php
@@ -1,9 +1,13 @@
 <?php 
 session_start(); 
 ?> 
+<?php
+$group=mysql_query("SELECT usergroup FROM homepage WHERE username=$username")
 
+
+?>
 <?php 
-if(!isset($_SESSION["username"])) 
+if(!isset($_SESSION["username"] $group=secure)) 
    { 
    echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
    exit; 
