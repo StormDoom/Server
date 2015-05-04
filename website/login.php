21de58102be7bc542812a2a337f9c03e3683cef4
StormDoom <josefkruse@googlemail.com>
2015-05-04 18:06:20 +0200

    Update login.php

--- website/login.php
+++ website/login.php
@@ -1,7 +1,19 @@
 <?php 
 session_start(); 
 ?> 
+<html>
+<form action="login.php" method="post">
+Dein Username:<br>
+<input type="text" size="24" maxlength="50"
+name="username"><br><br>
 
+Dein Passwort:<br>
+<input type="password" size="24" maxlength="50"
+name="password"><br>
+
+<input type="submit" value="Login">
+</form>
+</html>
 <?php 
 $verbindung = mysql_connect("localhost", "homepage" , "homepage") 
 or die("Verbindung zur Datenbank konnte nicht hergestellt werden"); 
