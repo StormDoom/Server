916524571310979a66c42a971327649a72b1f782
StormDoom <josefkruse@googlemail.com>
2015-05-04 23:06:32 +0200

    Update intern.php

--- website/intern.php
+++ website/intern.php
@@ -2,7 +2,7 @@
 session_start(); 
 ?> 
 <?php 
-
+$user = $_SESSION["username"];
 $dbhost = 'localhost';
 $dbuser = 'homepage';
 $dbpass = 'homepage';
@@ -13,7 +13,7 @@ if(! $conn )
   die('Could not connect: ' . mysql_error());
 }
 
-$sql = "SELECT usergroup FROM login WHERE username='Josef' ";
+$sql = "SELECT usergroup FROM login WHERE username=$user ";
 mysql_select_db('homepage');
 
 $result = mysql_query( $sql, $conn );
@@ -23,52 +23,23 @@ if(! $result )
   die('Could not get data: ' . mysql_error());
 }
 $usergroup = mysql_result($result, 0);
-echo $usergroup;
-echo "<br>";
-echo $usergroup;
-//while($row = mysql_fetch_array($retval, MYSQL_ASSOC))
-//{
-//  echo "ID: {$row['id']}  <br> ".
-//		"Name: {$row['username']} <br> ".
-//		"Passwd: {$row['password']} <br> ".
-//		"Group: {$row['usergroup']} <br> ".
-//		"--------------------------------<br>";
-//} 
-//echo "Fetched data successfully\n";
-//mysql_close($conn);
-//$row->usergroup = mysql_fetch_object($result);
-//echo $row->usergroup;
-//while($row = mysql_fetch_object($result))
-//{
-//	echo $row->username;
-//}
-//echo "<br>";
-//echo $retval;
-//echo "{$row['usergroup']}";
-	
-//echo "<br>";
-//if(!isset($_SESSION["username"])) 
-//{ 
-//echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
-//exit; 
-//}
- //  else
-//   {
- //     if($row->usergroup != "secure")
-   //   {
-     // echo "Du bist noch nicht freigeschaltet. Bitte gedulde dich etwas."
-   //   }
-  //      else
-    //     {
- //         echo $abfrage;
- //         echo "<br>";
- //         echo $ergebnis;
-//         echo "<br>";
-//          echo $row;
-//         echo "<br>";
-//          echo $_SESSION['username'];
-         //echo "Zum FTP geht es <a href=\"http://stormdoom.noip.me/ftp\">hier </a> lang.";
-         //}
-//   }
-  // exit; 
+
+if(!isset($_SESSION["username"])) 
+{ 
+echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
+exit; 
+}
+else
+{
+	if($usergroup != "registered")
+	{
+		echo "Du bist noch nicht freigeschaltet. Bitte gedulde dich etwas.";
+		
+	}
+	else
+	{
+		echo "Zum FTP geht es <a href=\"http://stormdoom.noip.me/ftp\">hier </a> lang.";
+	}
+}
+exit; 
 ?> 
