f724f0d4d0a965291eb85125093daf36850f2ced
StormDoom <josefkruse@googlemail.com>
2015-05-04 20:15:45 +0200

    Update intern.php

--- website/intern.php
+++ website/intern.php
@@ -7,8 +7,8 @@ session_start();
 //mysql_select_db("homepage") or die ("Datenbank konnte nicht ausgewählt werden"); 
 
 $abfrage = "SELECT usergroup FROM login WHERE username LIKE $_SESSION["username"] LIMIT 1";
-//$ergebnis = mysql_query($abfrage);
-//$row = mysql_fetch_object($ergebnis);
+$ergebnis = mysql_query($abfrage);
+$row = mysql_fetch_object($ergebnis);
 if(!isset($_SESSION["username"])) 
 { 
 echo "Bitte erst <a href=\"login.html\">einloggen</a>"; 
@@ -22,7 +22,9 @@ exit;
    //   }
         else
          {
-          echo "$abfrage";
+          echo $abfrage;
+          echo $ergebnis
+          echo $row
          //echo "Zum FTP geht es <a href=\"http://stormdoom.noip.me/ftp\">hier </a> lang.";
          }
 //   }
