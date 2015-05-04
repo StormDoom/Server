04f0cfa65d78566dcffd301be7a759bc71ea5ce6
StormDoom <josefkruse@googlemail.com>
2015-05-04 20:53:35 +0200

    Update intern.php

--- website/intern.php
+++ website/intern.php
@@ -5,12 +5,12 @@ session_start();
 //$verbindung = mysql_connect("localhost", "homepage" , "homepage");
 //mysql_select_db("homepage")
 ///or die ("Datenbank konnte nicht ausgewählt werden"); 
-echo $_SESSION['username'];
+     echo $_SESSION['username'];
 $username = $_SESSION['username'];
-echo "<br>";
-echo $username;
-
-// $abfrage = "SELECT usergroup FROM login WHERE username LIKE $username LIMIT 1";
+     echo "<br>";
+     echo $username;
+$abfrage = "SELECT usergroup FROM login WHERE username LIKE $username LIMIT 1";
+     echo $abfrage;
 //$ergebnis = mysql_query($abfrage);
 //$row = mysql_fetch_object($ergebnis);
 //echo "<br>";
